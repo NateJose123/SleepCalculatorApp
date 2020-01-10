17635942a89047d30a9443114c0b8fcb5551c531
Nathan Jose <nathan_jose@berea.edu>
2020-01-10 14:04:54 -0500

    Initial Commit

---
+++ Sleep Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sleep Calculator
+//
+//  Created by Nathan Jose on 1/10/20.
+//  Copyright © 2020 Nathan Jose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
